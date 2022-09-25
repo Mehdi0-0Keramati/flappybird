6c14578ba22e7c27371b28fd3e2e9a69fb01841d
MehdiKeramati <0mehdikeramati1@gmail.com>
2022-09-25 22:03:21 +0330

    add

---
+++ README.md
@@ -0,0 +1,1 @@
+see this app on here => https://Mehdi0-0Keramati.github.io/DinoGame
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import FlappyBird from "./Flappy.jsx";
+const App = () => {
+    return (
+        <>
+            <FlappyBird />
+        </>
+    );
+
+}
+
+export default App;
